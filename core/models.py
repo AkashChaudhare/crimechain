92a1b6d90cc8b1e98a1eff23137e02a9cb3108fe
AkashChaudhare <akash.chaudhare@gmail.com>
2023-02-17 22:21:03 +0530

    Updated

--- core/models.py
+++ core/models.py
@@ -17,6 +17,7 @@ class Convict(models.Model):
     education=models.CharField(max_length=100)
     financial_background=models.CharField(max_length=100)
 
+
 class Block(models.Model):
     def __str__(self):
         return self.perp.name + ' - '+ str(self.pk)
